311a8f8d913b0eac002a292e47d9622665740932
Kohki Takata <koki@Kokis-iMac.local>
2021-09-18 19:46:35 +0900

    add ApprovalCard.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 
 const App= () => {
 	return(
 		<div className = "ui container comments">
-			<CommentDetail author="Sam" />
-			<CommentDetail author="Alex"/>
-			<CommentDetail author="Jane" />
+			<ApprovalCard />
+			<CommentDetail 
+			  author="Sam" 
+			  timeAgo="Today at 4:45PM" 
+			  content="Nice blog post" 
+			  avatar={faker.image.avatar()}
+			 />
+			<CommentDetail 
+			  author="Alex" 
+			  timeAgo="Today at 2:00PM" 
+			  content="I like the subject"
+			  avatar={faker.image.avatar()}
+			 />
+			<CommentDetail 
+			  author="Jane" 
+			  timeAgo="Yesterday at 5:00PM" 
+			  content="I like writing" 
+			  avatar={faker.image.avatar()}
+			/>
 		</div>
 		);
 };
